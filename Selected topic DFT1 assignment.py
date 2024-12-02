9bb1c915952a6efa8a20eb6e86acaee9714de66f
Kargwak Abednego Sauri <110601008+Kasauri@users.noreply.github.com>
2024-12-02 16:17:23 +0100

    Add files via upload

--- Selected topic DFT1 assignment.py
+++ Selected topic DFT1 assignment.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 plt.close('all')
-L=10
+L=20
 x=np.linspace(0, L, 1000)
 
 def density(n):
@@ -18,8 +18,8 @@ def density(n):
         den+=(2/L)*(np.sin(i*np.pi*x/L))**2
     return den
 
-def norm(f):
-    return f/max(f)
+def norm(A):
+    return A/max(A)
 
 n1=norm(density(1))
 n2=norm(density(2))
@@ -29,29 +29,30 @@ n4=norm(density(100))
 plt.figure()
 plt.subplot(221)
 plt.plot(x, n1, "b", label="n=1")
-plt.ylabel("Density (a.u)")
-plt.xlabel("Distance x (a.u)")
+plt.ylabel("Total Density (a.u)")
+plt.xlabel("Distance L (a.u)")
 plt.legend()
 plt.grid()
 
 plt.subplot(222)
 plt.plot(x, n2, "r", label="n=2")
-plt.ylabel("Density (a.u)")
-plt.xlabel("Distance x (a.u)")
+plt.ylabel("Total Density (a.u)")
+plt.xlabel("Distance L (a.u)")
 plt.legend()
 plt.grid()
 
 plt.subplot(223)
 plt.plot(x, n3, "b", label="n=10")
-plt.ylabel("Density (a.u)")
-plt.xlabel("Distance x (a.u)")
+plt.ylabel("Total Density (a.u)")
+plt.xlabel("Distance L (a.u)")
 plt.legend()
 plt.grid()
 
 plt.subplot(224)
 plt.plot(x, n4, "r", label="n=100")
-plt.ylabel("Density (a.u)")
-plt.xlabel("Distance x (a.u)")
+plt.ylabel("Total Density (a.u)")
+plt.xlabel("Distance L (a.u)")
 plt.legend()
 plt.grid()
 plt.tight_layout()
+plt.show()
